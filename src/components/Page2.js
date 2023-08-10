1f47560321f44b1824d1a75abfb795537098ea78
haeun1 <87661039+haeun1@users.noreply.github.com>
2023-08-11 00:50:05 +0900

    commit

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,7 +1,8 @@
 // Page2.js
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Container, CenteredImage, Title, Divider } from "./TitleStyle";
+import { Container, CenteredImage, Title } from "./TitleStyle";
+import {Number,QuizBox,BlackText}from "./Page2Style"
 
 const Page2 = () => {
   const pageNumber = 2; // 페이지 번호를 변수로 설정
@@ -9,7 +10,12 @@ const Page2 = () => {
     <Container>
       <CenteredImage src="img/Transmeme.png" />
       <Title>신조어테스트</Title>
-      <Divider />
+      <Number>
+        <span>0</span>1
+      </Number>
+      <QuizBox>
+        <BlackText>검정색 글씨</BlackText>
+      </QuizBox>
       <NavLink to="/">메인 화면으로 이동</NavLink>
     </Container>
   );
